ae6a8ec1cd66d4f639db24dabdb459ba188bd8ca
Michael Ambrus <michael.ambrus@gmail.com>
2014-02-02 17:34:09 +0100

    work improvements 1
    Change-Id: Ia8d2d3b7eb920273c7305f12aef90bc642c1cba9

--- run.sh
+++ run.sh
@@ -11,14 +11,48 @@ XPRA_MIN=20
 XPRA_MAX=1000
 
 # Note: The following can be 500 on older UNIX systems. Might need tweaking until
-# autodetecting is complete. TBD
+# auto-detecting is complete. TBD
 UID_START=1000
+
+# Test if certain binary is installed
+#
+function test_installed () {
+	local BINARY=${1-""}
+	local RHOST=${2-""}
+	if [ "X${RHOST}" == "X" ]; then
+		if [ "X$(ssh ${RHOST} which ${BINARY})" == "X" ]; then
+			echo "The executable [${BINARY}] is not installed/or not" \
+				"in \$PATH on localhost" 1>&2
+			return 1
+		fi
+	else
+		RS=$(expr match "$(ssh ${RHOST} "bash -ic 'which '${BINARY}'' 2>/dev/null" | tail -n1)" ".*\(${BINARY}\)")
+		if [ "X${RS}" == "X" ]; then
+			echo "The executable [${BINARY}] is not installed/or not" \
+				"in \$PATH on host [${RHOST}]" 1>&2
+			return 1
+		fi
+	fi
+	return 0
+}
+
+function check_prereq() {
+	test_installed screen      ${RHOST} && \
+	test_installed xpra        ${RHOST} && \
+	test_installed xpra.run.sh ${RHOST} && return 0
+
+	echo "Please install it, then try again." 1>&2
+	return 1
+}
 
 function run() {
 	source .xpra..func.sh
-	start_screen
+	if [ "X${START_HIDDEN}" != "Xyes" ]; then
+		start_screen
+	fi
 
 	if [ "X${RHOST}" == "X" ]; then
+		echo "Staring a local xpra on host [$(hostname)]..." 1>&2
 
 		XPRA_SESSIONS=$(xpra_list_sessions)
 
@@ -43,7 +77,7 @@ function run() {
 
 		XPRA_DISPLAY=${XPRA_DISPLAY-"${USE_XPRA_DISPLAY}"}
 
-		# Deduct an application ID for the applicatin. Useful if several
+		# Deduct an application ID for the application. Useful if several
 		# instances of the same application is  used and you need an easy way
 		# to distinguish between them. Number for the last application will
 		# always be the last number used + 1.
@@ -70,19 +104,24 @@ function run() {
 
 		sleep 1
 	else
+		echo "Staring a remote xpra on [$RHOST] from this host [$(hostname)]..." 1>&2
+		# Starting a remote xpra session.
+
 		RLINE=$(grep ${RHOST} ${S3XPRA_DIR}/remotes)
 		if [ "X${RLINE}" == "X" ]; then
 			echo $RHOST >> ${S3XPRA_DIR}/remotes
 		fi
-		echo "Starting [xpra.run.sh ${REMOTE_ARGS}] @ [$RHOST]..."
+		echo "Starting [xpra.run.sh ${REMOTE_ARGS}] @ [$RHOST]..." 1>&2
 
 		XPRA_DISPLAY=$(
 			(ssh $RHOST -- "export PATH=$PATH:~/bin/; xpra.run.sh ${REMOTE_ARGS}" 2>&1) | \
 				egrep "\.log$" | sed -e 's/\.log$//' | sed -e 's/.*-//'
 		)
-		echo "Started on remote host at diplay [$XPRA_DISPLAY]."
+		echo "Started on remote host at diplay [$XPRA_DISPLAY]." 1>&2
 	fi
 
+	# Note that the screen daemon on local side is handling attaching to
+	# xpra.
 	if [ "X${START_HIDDEN}" != "Xyes" ]; then
 
 		if [ "X${RHOST}" == "X" ]; then
@@ -95,7 +134,7 @@ function run() {
 				-S ${SCREEN_SESSION_NAME} \
 				-p0 \
 				-X stuff "xpra attach ssh:${RHOST}:${XPRA_DISPLAY} --title=\"@title@ (${SESSION_NAME}) on @client-machine@\" &"`echo -ne '\015'`
-			echo "Session should turn up on your display shortly."
+			echo "Session should turn up on your display shortly." 1>&2
 		fi
 	fi
 }
@@ -105,7 +144,17 @@ if [ "$RUN_SH" == $( ebasename $0 ) ]; then
 	#Not sourced, do something with this.
 
 	RUN_SH_INFO=${RUN_SH}
+	echo "Session starting, Please wait..." 1>&2
 	source .xpra.ui..run.sh
+
+	if [ "X${TRUST_PREREQ}" != "Xyes" ]; then
+		echo "Prerequisites check..." 1>&2
+		check_prereq || (
+			echo "Prerequisites check failed. Can't continue" 1>&2
+			exit 1
+		)
+		echo "Prerequisites OK" 1>&2
+	fi
 
 	run "$@"
 	RC=$?
--- ui/.run.sh
+++ ui/.run.sh
@@ -1,35 +1,42 @@
 # UI part of rg.run.sh
 # This is not even a script, stupid and can't exist alone. It is purely
-# ment for beeing included.
+# meant for being included.
 
 #some defaults
 
 function print_run_help() {
-	#clear
-			cat <<EOF
-Usage: $RUN_SH_INFO [options] any-x-program
+	cat <<EOF
+Usage: $(basename $0) [options] any-x-program
 
 Uses xpra to run a server with *one* attached application and to make it
-visible on the current SCREEN.
+visible on the current display (or display of your choice).
 
-Options. Defautls within []:
-  -H              Start hidden. This is the default xpra behaviour.
+Options. Defautls within "[]":
+  -R <remote>     Run application on remote host and (normally) attach to this.
+  -t              Trust prerequisits. I.e. don't check. This makes start on
+                  remote hosts faster. Default is to check.
+  -H              Start hidden. This is the default xpra behaviour but
+                  $(basename $0) defaults to the opposite i.e. visible.
   -d <display>    xpra DISPLAY. Defaults to the highest number in use +1.
   -D <display>    Physical X-display. Defaults to \$DISPLAY [$DISPLAY]. I.e.
                   you can preset this by altering the \$DISPLAY environment
                   variable before calling this script if all operations should
-                  default so something else.
-  -R <remote>     Run application on remote host and (normally) attach to this.
+				  default so something else. This is useful mainly if host
+                  displayinig sessions have more than one physical displays.
 
 Example:
+  #Start a xprad firefox on localhost
   $RUN_SH_INFO firefox
+
+  #Start firefox on remote host as xprad session
+  $RUN_SH_INFO -R firefox
 
 EOF
 }
 
 	ORIG_ARGS="$@"
 
-	while getopts hHD:R: OPTION; do
+	while getopts hHD:R:td: OPTION; do
 		case $OPTION in
 		h)
 			print_run_help $0
@@ -44,11 +51,14 @@ EOF
 		D)
 			DISPLAY=$OPTARG
 			;;
+		t)
+			TRUST_PREREQ='yes'
+			;;
 		R)
 			RHOST=$OPTARG
 			REMOTE_ARGS=${ORIG_ARGS/-R/}
 			REMOTE_ARGS=${REMOTE_ARGS/$RHOST/}
-			REMOTE_ARGS="-H ${REMOTE_ARGS}"
+			REMOTE_ARGS="-Ht ${REMOTE_ARGS}"
 			;;
 		?)
 			echo "Syntax error:" 1>&2
@@ -67,9 +77,11 @@ EOF
 	fi
 
 	START_HIDDEN=${START_HIDDEN-"no"}
+	TRUST_PREREQ=${TRUST_PREREQ-"no"}
 	DISPLAY=${DISPLAY-":0"}
 	RHOST=${RHOST-""}
-	
+
 	#echo "${REMOTE_ARGS}"
+	echo "${TRUST_PREREQ}"
 	#exit 0
 
